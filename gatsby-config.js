e7977abe1ecbcae8dbcffb6cd61be08d72b42071
pch2013 <pch2103@gmail.com>
2020-04-16 17:55:14 +0300

    Gatsby - test Work WP connect

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `CHE testing Gatsby`,
+    description: `CHE testing Gatsby`,
+    author: `@CHE`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        // I have created a dummy site for us to use with the plugins we discussed
+        baseUrl: "localhost:8001",
+        protocol: "http",
+        hostingWPCOM: false,
+        // We will be using some advanced custom fields
+        useACF: true,
+        verboseOutput: true,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
